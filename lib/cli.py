4d4171cb8a080d03e1bf0b1b68618d4d7767ffd3
Louiza Mak <louiza.mak@gmail.com>
2024-05-23 17:49:13 -0500

    refactor nested while loops to separate menu methods

--- lib/cli.py
+++ lib/cli.py
@@ -25,81 +25,84 @@ def main():
         menu()
         choice = input("> ")
         if choice == "0":
-            # Sub Menu 1
-            sub_menu_1 = True
-            while sub_menu_1 == True:
-                list_pantries()
-                pantry_menu()
-                choice = input("> ")
-                if choice == "0":
-                    list_pantries()
-                    pantry = choose_pantry()
-                    if pantry:
-                        # Sub Menu 2
-                        sub_menu_2 = True
-                        while sub_menu_2 == True:
-                            prLightPurple(f" \n--- {pantry.owner}'s Pantry ---")
-                            list_foods(pantry.id)
-                            owner_menu()
-                            choice = input("> ")
-                            if choice == "0":
-                                update_owner(pantry.id)
-                            elif choice == "1":
-                                delete_pantry(pantry.id)
-                                sub_menu_2 = False
-                            elif choice == "2":
-                                add_food(pantry.id)
-                            elif choice == "3":
-                                remove_food(pantry.id)
-                            elif choice == "4":
-                                selected_food = select_food(pantry.id)
-                                if selected_food:
-                                    # Sub Menu 3
-                                    sub_menu_3 = True
-                                    while sub_menu_3 == True:
-                                        prLightPurple(f" \n--- Updating {selected_food.name} ---")
-                                        print(f"Stored Quantity: {selected_food.qty} \nCategory: {selected_food.type}")
-                                        food_update_menu(pantry.owner)
-                                        choice = input("> ")
-                                        if choice == "0" or choice == "1" or choice == "2":
-                                            update_food(selected_food, choice)
-                                        elif choice == "3":
-                                            sub_menu_3 = False
-                                        else:
-                                            prRed("Invalid choice.")
-                            elif choice == "5":
-                                # Sub Menu 4
-                                sub_menu_4 = True
-                                while sub_menu_4 == True:
-                                    category_list(pantry.id)
-                                    category_menu(pantry.owner)
-                                    choice = input("> ")
-                                    if choice == "0":
-                                        sub_menu_4 = False
-                                        sub_menu_4 = True
-                                    elif choice == "1":
-                                        sub_menu_4 = False
-                                    else: 
-                                        prRed("Invalid choice.")
-                            elif choice == "6":
-                                sub_menu_2 = False
-                            else:
-                                prRed("Invalid choice.")
-                elif choice == "1":
-                    add_pantry()
-                elif choice == "2":
-                    delete_pantry()
-                elif choice == "3":
-                    prGreen(" \nReturning to main menu.")
-                    sub_menu_1 = False
-                else:
-                    prRed("Invalid choice.")
+            while True:
+                sub_1()
         elif choice == "1":
             list_all_foods()
         elif choice == "2":
             exit_program()
         else:
             prRed("Invalid choice.")
+
+def sub_1():
+    list_pantries()
+    pantry_menu()
+    choice = input("> ")
+    if choice == "0":
+        list_pantries()
+        pantry = choose_pantry()
+        if pantry:
+            while True:
+                sub_2(pantry)
+    elif choice == "1":
+        add_pantry()
+    elif choice == "2":
+        delete_pantry()
+    elif choice == "3":
+        prGreen(" \nReturning to main menu.")
+        main()
+    else:
+        prRed("Invalid choice.")
+
+def sub_2(pantry):
+    prLightPurple(f" \n--- {pantry.owner}'s Pantry ---")
+    list_foods(pantry.id)
+    owner_menu()
+    choice = input("> ")
+    if choice == "0":
+        update_owner(pantry.id)
+    elif choice == "1":
+        delete_pantry(pantry.id)
+        sub_1()
+    elif choice == "2":
+        add_food(pantry.id)
+    elif choice == "3":
+        remove_food(pantry.id)
+    elif choice == "4":
+        selected_food = select_food(pantry.id)
+        if selected_food:
+            while True:
+                sub_3(pantry, selected_food)
+    elif choice == "5":
+        while True:
+            sub_4(pantry)
+    elif choice == "6":
+        sub_1()
+    else:
+        prRed("Invalid choice.")
+
+def sub_3(pantry, selected_food):
+    prLightPurple(f" \n--- Updating {selected_food.name} ---")
+    print(f"Stored Quantity: {selected_food.qty} \nCategory: {selected_food.type}")
+    food_update_menu(pantry.owner)
+    choice = input("> ")
+    if choice == "0" or choice == "1" or choice == "2":
+        update_food(selected_food, choice)
+    elif choice == "3":
+        sub_2(pantry)
+    else:
+        prRed("Invalid choice.")
+
+def sub_4(pantry):
+    category_list(pantry.id)
+    category_menu(pantry.owner)
+    choice = input("> ")
+    if choice == "0":
+        sub_4(pantry)
+    elif choice == "1":
+        sub_2(pantry)
+    else: 
+        prRed("Invalid choice.")
 
 
 def menu():
