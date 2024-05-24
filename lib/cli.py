ca4e5a9428202d0399fcd30c9162fb25a717da95
Louiza Mak <louiza.mak@gmail.com>
2024-05-24 12:17:06 -0500

    update method names to be descriptive

--- lib/cli.py
+++ lib/cli.py
@@ -20,51 +20,51 @@ def prLightPurple(str): print("\033[94m {}\033[00m" .format(str))
 def prGreen(str): print("\033[92m {}\033[00m" .format(str))
 def prRed(str): print("\033[91m {}\033[00m" .format(str))
 
-def main():
+def create_main_menu():
     while True:
-        menu()
+        main_menu()
         choice = input("> ")
         if choice == "1":
             while True:
-                sub_1()
+                create_pantry_menu()
         elif choice == "2":
             list_all_foods()
         elif choice == "3":
             exit_program()
         else:
             prRed("Invalid choice.")
-            main()
+            create_main_menu()
 
-def sub_1():
+def create_pantry_menu():
     list_pantries()
-    pantry_menu()
+    pantry_menu_list()
     choice = input("> ")
     if choice == "1":
         pantry = choose_pantry()
         if pantry:
             while True:
-                sub_2(pantry)
+                create_owner_menu(pantry)
     elif choice == "2":
         add_pantry()
     elif choice == "3":
         delete_pantry()
     elif choice == "4":
         prGreen(" \nReturning to main menu.")
-        main()
+        create_main_menu()
     else:
         prRed("Invalid choice.")
-        sub_1()
+        create_pantry_menu()
 
-def sub_2(pantry):
+def create_owner_menu(pantry):
     prLightPurple(f" \n--- {pantry.owner}'s Pantry ---")
     list_foods(pantry)
-    owner_menu()
+    owner_menu_list()
     choice = input("> ")
     if choice == "1":
         update_owner(pantry)
     elif choice == "2":
         delete_pantry(pantry)
-        sub_1()
+        create_pantry_menu()
     elif choice == "3":
         add_food(pantry)
     elif choice == "4":
@@ -73,43 +73,43 @@ def sub_2(pantry):
         selected_food = select_food(pantry)
         if selected_food:
             while True:
-                sub_3(pantry, selected_food)
+                create_food_update_menu(pantry, selected_food)
     elif choice == "6":
         while True:
-            sub_4(pantry)
+            create_category_menu(pantry)
     elif choice == "7":
-        sub_1()
+        create_pantry_menu()
     else:
         prRed("Invalid choice.")
-        sub_2(pantry)
+        create_owner_menu(pantry)
 
-def sub_3(pantry, selected_food):
+def create_food_update_menu(pantry, selected_food):
     prLightPurple(f" \n--- Updating {selected_food.name} ---")
     print(f"Stored Quantity: {selected_food.qty} \nCategory: {selected_food.type}")
-    food_update_menu(pantry.owner)
+    food_update_menu_list(pantry.owner)
     choice = input("> ")
     if choice == "1" or choice == "2" or choice == "3":
         update_food(selected_food, choice)
     elif choice == "4":
-        sub_2(pantry)
+        create_owner_menu(pantry)
     else:
         prRed("Invalid choice.")
-        sub_3(pantry, selected_food)
+        create_food_update_menu(pantry, selected_food)
 
-def sub_4(pantry):
+def create_category_menu(pantry):
     category_list(pantry)
-    category_menu(pantry.owner)
+    category_menu_list(pantry.owner)
     choice = input("> ")
     if choice == "1":
-        sub_4(pantry)
+        create_category_menu(pantry)
     elif choice == "2":
-        sub_2(pantry)
+        create_owner_menu(pantry)
     else: 
         prRed("Invalid choice.")
-        sub_4(pantry)
+        create_category_menu(pantry)
 
 
-def menu():
+def main_menu():
     prLightPurple("\n\
 ██████╗░░█████╗░███╗░░██╗████████╗██████╗░██╗░░░██╗  ░█████╗░██╗░░░░░██╗\n\
 ██╔══██╗██╔══██╗████╗░██║╚══██╔══╝██╔══██╗╚██╗░██╔╝  ██╔══██╗██║░░░░░██║\n\
@@ -123,7 +123,7 @@ def menu():
     print("3 - Exit")
 
 # Sub Menu 1
-def pantry_menu():
+def pantry_menu_list():
     print(" \nPlease select an option:")
     print("1 - Choose an existing pantry")
     print("2 - Add a pantry")
@@ -131,7 +131,7 @@ def pantry_menu():
     print("4 - Return to main menu")
 
 # Sub Menu 2
-def owner_menu():
+def owner_menu_list():
     print(" \nPlease select an option:")
     print("1 - Rename pantry")
     print("2 - Delete this pantry")
@@ -142,7 +142,7 @@ def owner_menu():
     print("7 - Return to pantry menu")
 
 # Sub Menu 3
-def food_update_menu(name):
+def food_update_menu_list(name):
     print(" \nPlease select an option:")
     print("1 - Update name")
     print("2 - Update quantity")
@@ -150,10 +150,10 @@ def food_update_menu(name):
     print(f"4 - Return to {name}'s pantry menu")
 
 # Sub Menu 4
-def category_menu(name):
+def category_menu_list(name):
     print(" \nPlease select an option:")
     print("1 - List foods from another category")
     print(f"2 - Return to {name}'s pantry menu")
 
 if __name__ == "__main__":
-    main()
+    create_main_menu()
